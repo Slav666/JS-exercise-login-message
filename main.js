b7ac9c597f94e326a6b1cd14ca7f9dcea06a1942
Slav666 <slawomirdyk@gmail.com>
2019-09-22 12:19:26 +0100

    Add focus and blur event

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const input = document.getElementById('pass');
 const password = "user";
-const message = "Hello, Magda here";
-
+const message = "Masz zupe w lodowce"
 const div = document.querySelector('div');
+
 input.addEventListener('input', (e) => {
     if (password === e.target.value)  {
         div.textContent = message;
+        e.target.value = "";
     }
     else {
-        div.textContent = "You have typed wrong password!"
+        div.textContent = "";
     }
 })
+
+input.addEventListener('focus', (e)=> {
+e.target.classList.add('active');
+})
+input.addEventListener('blur', (e)=> {
+    e.target.classList.remove('active');
+    })
+    
