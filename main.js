935d32cbb49552d06815b46c7b7c3f9df2a2f66d
Slav666 <slawomirdyk@gmail.com>
2019-09-22 13:14:29 +0100

    Add more passwords and messeges

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const input = document.getElementById('pass');
-const password = "user";
-const message = "Masz zupe w lodowce"
+const passwords = ["user", "spring", "John"];
+const messages = ["I've prepared sandwitches for you", "Spring is the best time of the year", "John has got a nice car"]
 const div = document.querySelector('div');
 
 input.addEventListener('input', (e) => {
-    if (password === e.target.value)  {
-        div.textContent = message;
-        e.target.value = "";
-    }
-    else {
-        div.textContent = "";
-    }
+    const text = e.target.value;
+    div.textContent = "";
+
+passwords.forEach((password, index) => {
+if (password === text) {
+    div.textContent = messages[index]
+}
 })
+})
+// input.addEventListener('input', (e) => {
+//     if (password === e.target.value)  {
+//         div.textContent = message;
+//         e.target.value = "";
+//     }
+//     else {
+//         div.textContent = "";
+//     }
+// })
 
 input.addEventListener('focus', (e)=> {
 e.target.classList.add('active');
