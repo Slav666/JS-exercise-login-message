5bd095828c1b1e0ea469a8775032b2eac88c4985
Slav666 <slawomirdyk@gmail.com>
2019-09-22 11:49:56 +0100

    Event listener works

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const input = document.getElementById('pass');
+const password = "user";
+const message = "Hello, Magda here";
 
+const div = document.querySelector('div');
 input.addEventListener('input', (e) => {
-    console.log(e.target.value);
+    if (password === e.target.value)  {
+        div.textContent = message;
+    }
+    else {
+        div.textContent = "You have typed wrong password!"
+    }
 })
